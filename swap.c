b7e921e07ad915d806533a42375a82389353ecd3
houssam5050 <hossamhamdan4545@gmail.com>
2025-07-16 15:45:12 +0200

    Update swap.c
    i used the pointer

--- swap.c
+++ swap.c
@@ -1,20 +1,6 @@
-#include <stdio.h>
-
-int main() {
-
-    int x = 5, y = 15, t;
-
-
-    t = x;
-    x = y;
-    y = t;
-
-
-
-     printf("after swap x = %d , y = %d ", x, y);
-
-
-
-
-    return 0;
+void swap(char*a, char*b){
+    char d = *a;
+    *a = *b;
+    *b = d;
+    
 }
